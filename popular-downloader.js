c7e3f0a1c373273fa90511fd3569117a949874e6
Christy Welsh <christyannwelsh@gmail.com>
2018-08-25 09:51:54 -0700

    complete with extensions

--- popular-downloader.js
+++ popular-downloader.js
@@ -1,21 +1,33 @@
-// Part 3
+// Part 3 -- Advanced
 
 var rp = require('request-promise');
-const path = require('path');
 const fs = require('fs');
 
+// request json data for popular reddit articles
 rp('https://reddit.com/r/popular.json')
     .then(res => {
 
-        // iterate over reddit data; create array of objects for 'data' level
+        // map over reddit data; create array of data objects found in 'data.children' array
         let articles = JSON.parse(res).data.children.map(item => item.data)
 
-        // map over articles, find videos, images, and gifs; is_video marked true for videos, post_hint marked image for images &
-        // gifs have reddit_video_preview prop
+        // map again, find images, videos, or gifs (url begins with i or v, or contains gif in each article's main url)
         articles.map(article => {
-            if (article.is_video || article.post_hint == "image" || (article.preview && article.preview.reddit_video_preview)) {
-                rp(article.url).pipe(fs.createWriteStream(`./downloads/${article.id}`))
-            }
-        })
+            let img = /\/\/i/
+            let vid = /\/\/v/
+            let gifv = /gifv/
+            let gif = /gif/
 
+            if (gifv.test(article.url)) {
+                // set readable url for gifvs & videos (main url does not open them)
+                let url =article.preview.reddit_video_preview.fallback_url
+                rp(url).pipe(fs.createWriteStream(`./downloads/${article.id}.gifv`))
+            } else if (gif.test(article.url)) {
+                rp(article.url).pipe(fs.createWriteStream(`./downloads/${article.id}.gif`))
+            } else if (vid.test(article.url)) {
+                let url =article.media.reddit_video.fallback_url
+                rp(url).pipe(fs.createWriteStream(`./downloads/${article.id}.mp4`))
+            } else if (img.test(article.url)) {
+                rp(article.url).pipe(fs.createWriteStream(`./downloads/${article.id}.jpg`))
+            }          
+        })
     }).catch(err => console.log(err))
